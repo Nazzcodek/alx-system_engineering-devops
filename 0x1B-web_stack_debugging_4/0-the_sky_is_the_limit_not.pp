249b7a0304e97fa8fd7a0b08f24c7fdda80d1789
Nazzcodek <bellnas09@gmail.com>
2023-11-16 10:08:47 +0000

    this sky is the not

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Nginx sever lrequest limit increment
+# Nginx sever request limit increment
 
 # change the ULIMIT  value in /etc/default/nginx
-exec {'ulimit-value':
+exec { 'ulimit-value':
   command => 'sed -i "s/15/5089/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx
-exec {'nginx-restart':
+exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
   require => Exec['ulimit-value'],
