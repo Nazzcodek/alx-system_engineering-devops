e6c196c1636457ce8f63287ce4df95bedb43bb0d
Nazzcodek <bellnas09@gmail.com>
2023-11-19 08:02:13 +0000

    advance

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # Nginx sever request limit increment
 
 # change the ULIMIT  value in /etc/default/nginx
-exec { 'ulimit-value':
-  command => 'sed -i "s/15/5089/" /etc/default/nginx',
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
@@ -10,5 +10,4 @@ exec { 'ulimit-value':
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
-  require => Exec['ulimit-value'],
 }
